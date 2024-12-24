acb96e7d3b290495921fd2d2250c170ec04e79ca
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-24 13:41:52 +0530

    I use while loop

--- javaBasics.java
+++ javaBasics.java
@@ -196,16 +196,162 @@
 //         }
 //     }  this is a solution
   
-  import java.util.*;
-  public class javaBasics{
-    public static void main(String args[]){
-        String a = "*";
-        for(int c = 0; c <= 5 ; c++ ){
-            for(int r = 5; r > 0 ; r--){
-                System.out.println(a);
-                a += "*";
-            }
-        }
+//   import java.util.*;
+//   public class javaBasics{
+//     public static void main(String args[]){
+//         String a = "*";
+//         for(int c = 0; c <= 5 ; c++ ){
+//             for(int r = 5; r > 0 ; r--){
+//                 System.out.println(a);
+//                 a += "*";
+//             }
+//         }
           
+//         }
+//     }  
+
+// import java.util.*;
+// public class javaBasics{
+//     public static void main(String args[]){
+//         int A = 1 , B = 3;
+
+//         if( A >= B ){
+//             System.out.print("A is gretar than B");
+//         }else{
+//             System.out.print("B is greater than A");
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class javaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the number of a ");
+//         int a = sc.nextInt();
+
+//         if( a % 2 == 0){
+//             System.out.print("The Number is even");
+//         }else{
+//             System.out.print("The Number is odd");
+//         }
+
+
+
+//     }
+// }
+
+
+// import java.util.*;
+// public class javaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.print("Enter the Income");
+//         int income = sc.nextInt();
+
+//         if(income <= 500000){
+//             System.out.print("0 tax");
+//         }else if (income >500000 && income <= 1000000){
+//             System.out.print("20% tax");
+//         }else {
+//             System.out.print("30% tax");
+//         }
+//     }
+// }
+
+
+
+// Ternary Operator
+
+// import java.util.*;
+// public class javaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the your marks");
+//         int marks = sc.nextInt();
+
+//         String result = (marks > 33) ? "Pass" : "Fail";
+//         System.out.print(result);
+//     }
+// }
+
+
+//                     Positive or Negative  program using if-else
+
+
+// import java.util.*;
+// public class javaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the number a");
+//         int a = sc.nextInt();
+
+//         if(a > 0){
+//             System.out.print("positive ");
+//         }else{
+//             System.out.print(" negative");
+//         }
+//     }
+// } 
+// import java.util.*;
+// public class Solution {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//         double temp = 103.5;
+//         if(temp >= 100){
+//             System.out.print(" You have a feve");
+//         }else{
+//             System.out.print(" You don't have a fever.");
+//         }
+// }
+// }
+
+
+// import java.util.*;
+
+// public class javaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the week number");
+//         int weekNum = sc.nextInt();
+
+//         switch(weekNum){
+//             case 1 : System.out.print("Sunday");
+//                         break;
+//             case 2 :  System.out.print("Monday");
+//                         break;           
+//             case 3 :  System.out.print("Tuesday"); 
+//                         break;           
+//             case 4 :  System.out.print("Wednesday");
+//                         break;            
+//             case 5 :  System.out.print("Thursday") ;
+//                         break;           
+//             case 6 :  System.out.print("Friday") ;
+//                         break;           
+//             case 7 :  System.out.print("Saturday");
+//                         break;
+//             default: System.out.print("this is not a week number");                        
+//         }
+
+//     }
+// }
+
+import java.util.*;
+
+public class javaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int num = 0;
+
+        while( num <= 10 ){
+            System.out.println(num);
+            num++;
         }
-    }  
+    }
+}
