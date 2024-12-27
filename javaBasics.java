fa2ef3587e9d06c6e23f9e797efe9dea33cdf6aa
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-27 06:05:11 +0530

    Check If Number Is Prime Or Not using function

--- javaBasics.java
+++ javaBasics.java
@@ -621,16 +621,38 @@ public class javaBasics{
 
         // function to calculet sum  of 2 numbers
 
+        // public static int sum(int a, int b){
+        //     return a + b;
+        // }
+        // // function to calculet sum of 3 numbers
+        // public static int sum(int a, int b , int c){
+        //     return  a + b + c;
+        // }
+
         public static int sum(int a, int b){
-            return a + b;
+            return a+b;
         }
-        // function to calculet sum of 3 numbers
-        public static int sum(int a, int b , int c){
-            return a + b + c;
+        public static Float sum(Float a , Float b){
+            return a+b;
+        }
+
+        public static Boolean isPrime(int n){
+            if (n == 2){
+                return true;
+            }
+            Boolean isPrime = true;
+            for(int i = 2 ; i <= n-1 ; i++){
+                if (n % 2 == 0){
+                    isPrime = false;
+                    break;
+                }
+               
+            }
+             return isPrime;
         }
 
     public static void main(String args[]){
-        System.out.println(sum(5, 5));
-        System.out.print(sum(5, 5, 5));
+        System.out.println(isPrime(2));
+       
     }
 }
