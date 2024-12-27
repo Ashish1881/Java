ebeaf76627bfe80c8f86eadd444f8ce0363e6c93
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-27 21:57:20 +0530

    convert Binary to decimal

--- javaBasics.java
+++ javaBasics.java
@@ -672,9 +672,24 @@ public class javaBasics{
                 // System.out.print();
             }
         }
+        public static void binToDec(int binNum){
+            int myNum = binNum;
+            int pow = 0;
+            int decNum = 0;
+
+            while(binNum > 0){
+                int lastDigit = binNum % 10;
+                decNum = decNum +(lastDigit * (int)Math.pow(2 , pow));
+
+                pow++;
+                binNum = binNum / 10;
+                 
+            }
+            System.out.print(" Decimal of " + myNum +" = " + decNum);
+        }
 
     public static void main(String args[]){
-       primesInRange(20);
+       binToDec(01);
        
     }
 }
