6c8ed2d5e55a10d704ba08a9b72b7c369fdecdce
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-27 21:02:28 +0530

    Prime numbers print

--- javaBasics.java
+++ javaBasics.java
@@ -664,8 +664,17 @@ public class javaBasics{
             return true;
         }
 
+        public static void primesInRange(int n){
+            for (int i = 2 ; i <= n ; i++){
+                if (isPrime(i)){
+                    System.out.print(i+" ");
+                }
+                // System.out.print();
+            }
+        }
+
     public static void main(String args[]){
-        System.out.println(isPrime(6));
+       primesInRange(20);
        
     }
 }
