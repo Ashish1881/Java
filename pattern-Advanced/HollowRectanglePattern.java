82491302163954eaa90ca95ffa0fea340b0710bb
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-29 12:04:02 +0530

    inverted half pyramid with numbers

--- pattern-Advanced/HollowRectanglePattern.java
+++ pattern-Advanced/HollowRectanglePattern.java
@@ -16,7 +16,38 @@ public class HollowRectanglePattern{
             System.out.println();
         }
     }
+   
+   public static void inverted_rotated_half_pyaramid(int n){
+    for(int i=1;  i<=n; i++){
+        // ourter
+
+        for(int j=1; j<=n-1; j++){
+            System.out.print(" ");
+        }
+
+        //Stars 
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+   
+    }
+ }
+
+ public static void numprint(int n){
+    for(int i=1; i<=n; i++){
+        
+        for(int j=1; j<=n-i+1; j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+ }
+        
     public static void main(String args[]){
-        pattern(4,4);
+        // pattern(4,4);
+       // inverted_rotated_half_pyaramid(5);
+       numprint(5);
     }
 }
