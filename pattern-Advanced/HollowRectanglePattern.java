eb2bd2a0c6db131043e5717068e5ce76c6868596
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-29 13:34:22 +0530

    Butterfly pattern

--- pattern-Advanced/HollowRectanglePattern.java
+++ pattern-Advanced/HollowRectanglePattern.java
@@ -69,13 +69,60 @@ public class HollowRectanglePattern{
         System.out.println(); 
     }
  }
+
+    public static void butterfly(int n){
+        // 1st half
+        for(int i=1; i<=n; i++){
+            
+            
+            //Stars -1
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces -2*(n-1)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //stars -i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 2nd half
+        for(int i=n; i>=1; i--){
+            //Stars -1
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces -2*(n-1)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //stars -i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }     
+
+        
         
     public static void main(String args[]){
         // pattern(4,4);
        // inverted_rotated_half_pyaramid(5);
-    //    numprint(5);
+        // numprint(5);
         //   number(5);
-        zero_one_pattern(5);
+        // zero_one_pattern(5);
+        butterfly(5);
 
     }
 }
