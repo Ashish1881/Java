2191f4b7215319e3ca874212d249ca18c41def8c
Ashish1881 <ashishvishwakarama950@gmail.com>
2024-12-29 12:40:35 +0530

    Zero one tringle pattern

--- pattern-Advanced/HollowRectanglePattern.java
+++ pattern-Advanced/HollowRectanglePattern.java
@@ -44,10 +44,38 @@ public class HollowRectanglePattern{
         System.out.println();
     }
  }
+
+ public static void number(int n){
+    int num = 1;
+    for(int i=1; i<=n ; i++){
+        for(int j=1; j<=i ; j++){
+            System.out.print(num + " ");
+            num++;
+        }
+        System.out.println();
+    }
+ }
+
+ public static void zero_one_pattern(int n) {
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            if((i+j) % 2 == 0){
+                System.out.print("1"+" ");
+            }else{
+                System.out.print("0"+" ");
+            } 
+                                                                                               
+        }
+        System.out.println(); 
+    }
+ }
         
     public static void main(String args[]){
         // pattern(4,4);
        // inverted_rotated_half_pyaramid(5);
-       numprint(5);
+    //    numprint(5);
+        //   number(5);
+        zero_one_pattern(5);
+
     }
 }
