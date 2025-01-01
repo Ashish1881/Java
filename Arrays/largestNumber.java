ead932790fd7481c9665199fd19b420ed028f7fc
Ashish1881 <ashishvishwakarama950@gmail.com>
2025-01-01 15:21:57 +0530

    find the smallest numbers in arrays

--- Arrays/largestNumber.java
+++ Arrays/largestNumber.java
@@ -2,12 +2,18 @@ import java.util.*;
 public class largestNumber{
    public static int largestNum(int numbers[]){
     int largest = Integer.MIN_VALUE;
+    int smallest = Integer.MAX_VALUE;
 
     for(int i=0; i<numbers.length; i++){
         if(largest < numbers[i]){
             largest = numbers[i];
         }
+        if(smallest > numbers[i]){
+            smallest = numbers[i];
+        }
+        
     }
+    System.out.println("smallest value is :" + smallest);
     return largest;
    }
    
