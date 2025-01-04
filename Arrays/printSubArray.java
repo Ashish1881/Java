78ae025ead0303a4bfd0c069ff13acfb0270af94
Ashish1881 <ashishvishwakarama950@gmail.com>
2025-01-04 15:04:24 +0530

    prefix array

--- Arrays/printSubArray.java
+++ Arrays/printSubArray.java
@@ -2,19 +2,24 @@ public class printSubArray{
     public static void subArray(int numbers[]){
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
 
+        prefix[0] = numbers[0];
+        // calculate prefix array
+        for(int i = 1; i<prefix.length; i++){
+                prefix[i] = prefix[i-1] + numbers[i];
+            }
 
         for(int i=0; i<numbers.length; i++){
             int start = i;
+
+            
+            
             for(int j=i; j<numbers.length; j++){
                 int end =j;
-                currSum = 0;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
             
-                for(int k=start; k<=end; k++){ //print
-                    //subarray sum
-                    currSum += numbers[k];
-                }
-                System.out.println(currSum);
+                
                if(maxSum < currSum){
                     maxSum = currSum;
                }
@@ -28,7 +33,7 @@ public class printSubArray{
     }
 
     public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10} ;
+        int numbers[] = {1,-2,6,-1,3} ;
         subArray(numbers);
     }
 }
